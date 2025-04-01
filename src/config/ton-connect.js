0b9c78022dca1324ec2d57ebc846449d685b8a46
MatthewAura444 <matthewaura444@gmail.com>
2025-04-01 12:42:07 +0300

    Clean repository and add Stars Market web app

--- src/config/ton-connect.js
+++
@@ -1,9 +0,0 @@
-const { TonConnect } = require('ton-connect');
-
-const tonConnect = new TonConnect({
-  manifestUrl: process.env.TON_CONNECT_MANIFEST_URL,
-  buttonRootId: 'ton-connect-button',
-  apiKey: process.env.TON_CONNECT_API_KEY
-});
-
-module.exports = tonConnect; 
